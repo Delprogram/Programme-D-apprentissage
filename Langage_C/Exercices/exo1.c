673d3dbb00c4d63552413244e9760b3bd884c0c2
tevaitea_doom <tevaitea.doom@coda-student.school>
2025-12-16 09:47:16 +0100

    exo 1

--- Langage_C/Exercices/exo1.c
+++ Langage_C/Exercices/exo1.c
@@ -4,4 +4,18 @@
     Prototype :
     void swap(int *a, int *b);
 */
+#include <stdio.h>
+void swap(int *a, int *b);
 
+void swap(int *a, int *b){
+    int conv_a;
+    conv_a = *a;
+    *a = *b;
+    *b = conv_a;
+}
+int main() {
+    int a = 5;
+    int b = 10;
+    swap( &a, &b);
+    printf("a = %d ,b =  %d ", a , b);
+}
---
+++ Langage_C/Exercices/exo2.c
@@ -0,0 +1,16 @@
+/* Consignes
+   Écrire une fonction qui retourne la somme des éléments d’un 
+   tableau en utilisant uniquement des pointeurs 
+   (sans utiliser les index []).
+*/
+#include <stdio.h>
+
+int somme_tableau();
+
+int somme_tableau(){
+
+}
+
+int main(){
+    
+}
